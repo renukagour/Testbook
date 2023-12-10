548603ebff86a05789f15f4e881d916191086aa5
Renuka_Gour <2110041@ritindia.edu>
2023-12-10 10:36:38 +0530

    upto javascript class 2

---
+++ Daily Classes/HTML_CSS_JS/javascript/demo.js
@@ -0,0 +1,7 @@
+document.getElementById('demo').innerHTML='<h1>Hello Students external file<h1>'
+
+// if sccript in head tag
+// document.addEventListener('DOMContentLoaded',()=>{
+//     document.getElementById('demo').innerHTML='<h1>Hello Students external file<h1>'
+  
+// })
