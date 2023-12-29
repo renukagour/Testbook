5588c9abac685b170f7bf56c5dcdc25a3f3387e0
Renuka_Gour <2110041@ritindia.edu>
2023-12-29 19:09:39 +0530

    change by me upto 29-12-23

---
+++ Daily Classes/HTML_CSS_JS/javascript/script.js
@@ -0,0 +1,6 @@
+document.getElementById("p1").innerHTML = "Hello JavaScript";
+// accessing the second paragraph and adding text color to it
+document.getElementById('p1').style.color='red';
+// accessing the first paragraph and modifying the font size of it
+document.getElementById('intro').style.fontSize='35px';// accessing the whole body elements of the HTML and adding thebackground color to it
+document.body.style.backgroundColor  = "yellow";
