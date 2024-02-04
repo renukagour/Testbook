ad39fc9101a20a027a25e42053ee41e4373140ff
Renuka_Gour <2110041@ritindia.edu>
2024-02-04 20:10:53 +0530

    changes done

---
+++ Daily Classes/DSA/Array.js
@@ -0,0 +1,52 @@
+// let fruits = ["apple", "banana ", "mango", "orange"];
+// console.log(fruits);
+// let mix = [5, "Hello", true]; //in js it can be diferent type
+// console.log(mix);
+
+//accessing
+// let arr = [2, 5, 77, 9];
+// console.log(arr[2]);
+
+// let marks = [98, 96, 94, 95, 92];
+// console.log(marks[4]);
+// console.log(marks);
+//modifying elements of aray
+// marks[4] = 98;
+// console.log(marks[4]);
+// console.log(marks);
+//length
+// console.log(marks.length);
+//Iterate in array
+// n=marks.length;
+// sum = 0;
+// for (i = 0; i < marks.length; i++)
+//     sum += marks[i];
+// avg=sum/n;
+// let per =sum*100/(n*100);
+// console.log(sum," ",avg," ",per);
+
+//sort methods
+
+// marks.sort();
+// console.log(marks);
+// console.log(marks.reverse());
+// console.log(marks);
+// let arr=[3,2,5,8,9,6,2];
+
+// console.log(arr);
+// console.log(arr.length);
+// arr.push(2);
+// console.log(arr);
+// console.log(arr.length);
+// arr.pop();
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr.indexOf(9));
+// console.log(arr.indexOf(10));//if element not present => -1
+// console.log(arr.indexOf(2));//if element present many times  => first occurence inedx
+// console.log(arr.lastIndexOf(2)); //last occurence index
+
+let a=[3,2,5,8,9,6,2];
+console.log(a.includes(90));
+console.log(a.includes(9));
+
